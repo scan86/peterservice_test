ec027d96a0c1a5364ed950111746803e39feeb2b
Dmitriy Anikin <danikin@creditnet.ru>
2016-08-12 20:50:01 +0300

    wip

--- src/main/java/com/creditnet/test/DemoApplication.java
+++ src/main/java/com/creditnet/test/DemoApplication.java
@@ -1,11 +1,9 @@
 package com.creditnet.test;
 
-import com.creditnet.test.service.PingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -13,7 +11,6 @@ public class DemoApplication {
 	private final static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
-        logger.debug("run");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
--- src/main/java/com/creditnet/test/controller/Controller.java
+++ src/main/java/com/creditnet/test/controller/Controller.java
@@ -2,6 +2,8 @@ package com.creditnet.test.controller;
 
 import com.creditnet.test.domain.Pong;
 import com.creditnet.test.service.PingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,22 +11,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 /**
  * Created by scan on 12.08.16.
  */
 @RestController
 public class Controller {
 
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
     @Autowired
     private PingService pingService;
 
-    @RequestMapping(path = "/ping", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, path = "/ping")
     public ResponseEntity<Pong> ping() {
+        logger.info("processing ping reqest");
         return new ResponseEntity<Pong>(
                 pingService.ping(),
                 HttpStatus.OK
         );
     }
+
 
 
 }
