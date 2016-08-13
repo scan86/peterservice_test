384ec6863f6c40eb5186ac7f901979604c875379
Dmitriy Anikin <danikin@creditnet.ru>
2016-08-13 16:24:16 +0300

    service mode

--- src/main/java/com/creditnet/test/service/PingService.java
+++ src/main/java/com/creditnet/test/service/PingService.java
@@ -1,6 +1,8 @@
 package com.creditnet.test.service;
 
 import com.creditnet.test.domain.Pong;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,8 @@ import java.time.LocalDateTime;
  */
 @Service
 public class PingService {
+
+    private final static Logger logger = LoggerFactory.getLogger(PingService.class);
 
     public Pong ping() {
         return new Pong("pong", LocalDateTime.now());
